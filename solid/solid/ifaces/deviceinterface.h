drfiemost/kdelibs

commit 170768cbca284ae4ca1734bc21cdb14dea9f67bf

cmake: use exported targets for Qt

solid/CMakeLists.txt
@@ -1,7 +1,5 @@
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR}
-                     ${CMAKE_CURRENT_BINARY_DIR}
-                     ${QT_INCLUDE_DIR}
-                     ${QT_QTCORE_INCLUDE_DIR} )
+                     ${CMAKE_CURRENT_BINARY_DIR} )
 
 # Cannot compile with enable-final because of too many "using namespace" in
 # .cpp files, leading to conflicts e.g. between

solid/solid/CMakeLists.txt
@@ -1,7 +1,6 @@
 set( EXECUTABLE_OUTPUT_PATH ${CMAKE_CURRENT_BINARY_DIR} )
 add_subdirectory( ifaces )
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR} )
-include_directories( ${QT_QTDBUS_INCLUDE_DIR} )
 
 if(WIN32)
    include_directories( ${KDEWIN_INCLUDES} )
@@ -383,7 +382,7 @@ qt4_add_dbus_interface(solid_LIB_SRCS org.kde.Solid.Networking.Client.xml
 set(solid_OPTIONAL_LIBS)
 
 if(WIN32)
-   set(solid_OPTIONAL_LIBS ${solid_OPTIONAL_LIBS} ${KDEWIN_LIBRARY} ${QT_QTNETWORK_LIBRARY})
+   set(solid_OPTIONAL_LIBS ${solid_OPTIONAL_LIBS} ${KDEWIN_LIBRARY} Qt4::QtNetwork)
    if(WITH_SOLID_WMI)
       set(solid_OPTIONAL_LIBS ${solid_OPTIONAL_LIBS} ${WBEM_LIBRARIES})
    else()
@@ -405,8 +404,8 @@ if ( UDEV_FOUND )
    set(solid_OPTIONAL_LIBS ${solid_OPTIONAL_LIBS} ${UDEV_LIBS})
 endif ( UDEV_FOUND )
 
-target_link_libraries(solid LINK_PRIVATE ${QT_QTCORE_LIBRARY} ${QT_QTDBUS_LIBRARY} ${QT_QTXML_LIBRARY} ${QT_QTGUI_LIBRARY} ${solid_OPTIONAL_LIBS} )
-target_link_libraries(solid LINK_PUBLIC ${QT_CORE_LIBRARY} )
+target_link_libraries(solid LINK_PRIVATE Qt4::QtCore Qt4::QtDBus Qt4::QtXml Qt4::QtGui ${solid_OPTIONAL_LIBS} )
+target_link_libraries(solid LINK_PUBLIC Qt4::QtCore)
 
 if (WINCE)
    target_link_libraries(solid LINK_PRIVATE ${WCECOMPAT_LIBRARIES})
@@ -427,7 +426,7 @@ install(FILES org.freedesktop.PowerManagement.Inhibit.xml DESTINATION ${DBUS_INT
 
 kde4_add_library(solid_static STATIC ${solid_LIB_SRCS})
 
-target_link_libraries(solid_static ${QT_QTCORE_LIBRARY} ${QT_QTDBUS_LIBRARY} ${QT_QTXML_LIBRARY} ${QT_QTGUI_LIBRARY} ${solid_OPTIONAL_LIBS})
+target_link_libraries(solid_static Qt4::QtCore Qt4::QtDBus Qt4::QtXml Qt4::QtGui ${solid_OPTIONAL_LIBS})
 
 if(WIN32)
    set_target_properties(solid_static PROPERTIES COMPILE_FLAGS -DSOLID_EXPORT=)

threadweaver/Weaver/CMakeLists.txt
@@ -1,4 +1,4 @@
-include_directories(${QT_INCLUDE_DIR} ${QT_QTCORE_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR})
+include_directories(${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR})
 
 if(WIN32)
    include_directories(${KDEWIN_INCLUDES})
@@ -31,8 +31,8 @@ set(ThreadWeaver_LIB_SRCS
 
 
 kde4_add_library(threadweaver ${LIBRARY_TYPE} ${ThreadWeaver_LIB_SRCS})
-target_link_libraries(threadweaver LINK_PRIVATE ${QT_QTCORE_LIBRARY})
-target_link_libraries(threadweaver LINK_PUBLIC ${QT_QTCORE_LIBRARY} )
+target_link_libraries(threadweaver LINK_PRIVATE Qt4::QtCore)
+target_link_libraries(threadweaver LINK_PUBLIC Qt4::QtCore)
 
 set_target_properties(threadweaver PROPERTIES
     VERSION ${GENERIC_LIB_VERSION}

CMakeLists.txt
@@ -1,9 +1,14 @@
-cmake_minimum_required(VERSION 3.1 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.1)
 project(kdelibs)
 
 set(CMAKE_CXX_EXTENSIONS OFF)
-set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR
+        "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    add_compile_options("$<$<CONFIG:DEBUG>:-Wall;-Wextra;-pedantic>")
+endif()
 
 # where to look first for cmake modules, before ${CMAKE_ROOT}/Modules/ is checked
 set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/modules")

dnssd/CMakeLists.txt
@@ -75,12 +75,12 @@ kde4_add_library(kdnssd ${LIBRARY_TYPE} ${kdnssd_LIB_SRCS})
 
 # KDE 5: don't link against KDEUI_LIBS (when settings.kfgc is changed)
 if (HAVE_DNSSD)
-  target_link_libraries(kdnssd LINK_PRIVATE ${KDNSSD_BASE_LIBS} ${DNSSD_LIBRARIES} ${QT_QTNETWORK_LIBRARY})
+  target_link_libraries(kdnssd LINK_PRIVATE ${KDNSSD_BASE_LIBS} ${DNSSD_LIBRARIES} Qt4::QtNetwork)
 else (HAVE_DNSSD)
-  target_link_libraries(kdnssd LINK_PRIVATE ${KDNSSD_BASE_LIBS} ${QT_QTNETWORK_LIBRARY})
+  target_link_libraries(kdnssd LINK_PRIVATE ${KDNSSD_BASE_LIBS} Qt4::QtNetwork)
 endif (HAVE_DNSSD)
 
-target_link_libraries(kdnssd  LINK_PUBLIC kdecore ${QT_QTCORE_LIBRARY} )
+target_link_libraries(kdnssd  LINK_PUBLIC kdecore Qt4::QtCore)
 
 set_target_properties(kdnssd PROPERTIES
    VERSION ${GENERIC_LIB_VERSION}

experimental/libkdeclarative/CMakeLists.txt
@@ -30,7 +30,8 @@ kde4_add_library(kdeclarative SHARED ${kdeclarative_LIB_SRCS})
 
 set_target_properties(kdeclarative PROPERTIES VERSION ${KDE_NON_GENERIC_LIB_VERSION} SOVERSION ${KDE_NON_GENERIC_LIB_SOVERSION})
 
-target_link_libraries(kdeclarative LINK_PRIVATE ${KDE4_KDECORE_LIBS} ${KDE4_KDEUI_LIBS} ${QT_QTSCRIPT_LIBRARY} ${QT_QTDECLARATIVE_LIBRARY})
+target_link_libraries(kdeclarative LINK_PRIVATE ${KDE4_KDECORE_LIBS} ${KDE4_KDEUI_LIBS} Qt4::QtScript
+ Qt4::QtDeclarative)
 
 
 ## test
@@ -39,7 +40,7 @@ set(kdeclarative_TEST_SRCS
 
 kde4_add_executable(kdeclarativetest ${kdeclarative_TEST_SRCS})
 
-target_link_libraries(kdeclarativetest kdeclarative ${KDE4_KDECORE_LIBS} ${QT_QTGUI_LIBRARY} ${QT_QTSCRIPT_LIBRARY} ${QT_QTDECLARATIVE_LIBRARY})
+target_link_libraries(kdeclarativetest kdeclarative ${KDE4_KDECORE_LIBS} Qt4::QtGui Qt4::QtScript Qt4::QtDeclarative)
 
 ## install
 

kdecore/CMakeLists.txt
@@ -410,7 +410,7 @@ endif (NOT Q_WS_X11 AND NOT Q_WS_QWS)
 
 kde4_add_library(kdecore ${LIBRARY_TYPE} ${kdecore_LIB_SRCS})
 
-target_link_libraries(kdecore LINK_PRIVATE ${QT_QTCORE_LIBRARY} ${QT_QTNETWORK_LIBRARY} ${QT_QTDBUS_LIBRARY} ${QT_QTXML_LIBRARY} ${ZLIB_LIBRARY} ${kdecore_OPTIONAL_LIBS})
+target_link_libraries(kdecore LINK_PRIVATE Qt4::QtCore Qt4::QtNetwork Qt4::QtDBus Qt4::QtXml ${ZLIB_LIBRARY} ${kdecore_OPTIONAL_LIBS})
 
 if(WINCE)
   target_link_libraries(kdecore LINK_PRIVATE ${WCECOMPAT_LIBRARIES} Ceshell.lib)
@@ -437,12 +437,12 @@ if(FAM_FOUND)
 endif(FAM_FOUND)
 
 if(WIN32)
-  set(kdecore_LINK_INTERFACE_LIBRARIES  ${QT_QTDBUS_LIBRARY} ${QT_QTCORE_LIBRARY} ${KDEWIN_LIBRARIES})
+  set(kdecore_LINK_INTERFACE_LIBRARIES  Qt4::QtDBus Qt4::QtCore ${KDEWIN_LIBRARIES})
 else(WIN32)
   if(APPLE)
-    set(kdecore_LINK_INTERFACE_LIBRARIES ${QT_QTDBUS_LIBRARY} ${QT_QTCORE_LIBRARY} ${CARBON_LIBRARY})
+    set(kdecore_LINK_INTERFACE_LIBRARIES Qt4::QtDBus Qt4::QtCore ${CARBON_LIBRARY})
   else(APPLE)
-    set(kdecore_LINK_INTERFACE_LIBRARIES ${QT_QTDBUS_LIBRARY} ${QT_QTCORE_LIBRARY} )
+    set(kdecore_LINK_INTERFACE_LIBRARIES Qt4::QtDBus Qt4::QtCore )
   endif(APPLE)
 endif(WIN32)
 
@@ -536,7 +536,7 @@ if(NOT WINCE)
 
     kde4_add_plugin( ktranscript ${ktranscript_LIB_SRCS} )
 
-    target_link_libraries( ktranscript kjs ${QT_QTCORE_LIBRARY} )
+    target_link_libraries( ktranscript kjs Qt4::QtCore )
 
 
     if(KDE4_ENABLE_FINAL)

kded/CMakeLists.txt
@@ -63,7 +63,7 @@ endif(KBUILDSYCOCA_NO_KCRASH)
 # We need to add a '4' so that kde3 apps running kbuildsycoca don't run the kde4 one.
 kde4_add_kdeinit_executable( kbuildsycoca4 NOGUI ${kbuildsycoca_KDEINIT_SRCS})
 
-target_link_libraries(kdeinit_kbuildsycoca4 ${KBUILDSYCOCA_MAIN_LIBS} ${QT_QTXML_LIBRARY})
+target_link_libraries(kdeinit_kbuildsycoca4 ${KBUILDSYCOCA_MAIN_LIBS} Qt4::QtXml)
 
 install(TARGETS kdeinit_kbuildsycoca4 ${INSTALL_TARGETS_DEFAULT_ARGS})
 

commit 7a3a3544bc819e141b5efb2463e430d321ae5e0f

Fix compiling broken with 170768c

solid/CMakeLists.txt
@@ -1,5 +1,7 @@
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR}
-                     ${CMAKE_CURRENT_BINARY_DIR} )
+                     ${CMAKE_CURRENT_BINARY_DIR}
+                     ${QT_INCLUDE_DIR}
+                     ${QT_QTCORE_INCLUDE_DIR} )
 
 # Cannot compile with enable-final because of too many "using namespace" in
 # .cpp files, leading to conflicts e.g. between

solid/solid/CMakeLists.txt
@@ -1,6 +1,7 @@
 set( EXECUTABLE_OUTPUT_PATH ${CMAKE_CURRENT_BINARY_DIR} )
 add_subdirectory( ifaces )
 include_directories( ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR} )
+include_directories( ${QT_QTDBUS_INCLUDE_DIR} )
 
 if(WIN32)
    include_directories( ${KDEWIN_INCLUDES} )

solid/solid/ifaces/deviceinterface.h
@@ -21,8 +21,8 @@
 #ifndef SOLID_IFACES_DEVICEINTERFACE_H
 #define SOLID_IFACES_DEVICEINTERFACE_H
 
-#include <QtCore/QObject>
-#include <QtCore/QBool>
+#include <QObject>
+#include <QBool>
 
 namespace Solid
 {

threadweaver/Weaver/CMakeLists.txt
@@ -1,4 +1,5 @@
 include_directories(${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR})
+include_directories(${QT_INCLUDE_DIR} ${QT_QTCORE_INCLUDE_DIR})
 
 if(WIN32)
    include_directories(${KDEWIN_INCLUDES})

threadweaver/Weaver/WeaverInterface.h
@@ -29,7 +29,7 @@
 #ifndef WeaverInterface_H
 #define WeaverInterface_H
 
-#include <QtCore/QObject>
+#include <QObject>
 
 #include <threadweaver/threadweaver_export.h>
 