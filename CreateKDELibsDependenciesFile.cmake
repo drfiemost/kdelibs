drfiemost/kdelibs

commit 975aebc0fe22ee7d5082a87556f6d222bca875dd

Fix cmake deprecated warning

cmake/modules/FindGSSAPI.cmake
@@ -33,17 +33,17 @@ else(GSSAPI_LIBS AND GSSAPI_FLAVOR)
   if(KRB5_CONFIG)
   
     set(HAVE_KRB5_GSSAPI TRUE)
-    exec_program(${KRB5_CONFIG} ARGS --libs gssapi RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GSSAPI_LIBS)
+    execute_process(COMMAND ${KRB5_CONFIG} --libs gssapi RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE GSSAPI_LIBS)
     if(_return_VALUE)
       message(STATUS "GSSAPI configure check failed.")
       set(HAVE_KRB5_GSSAPI FALSE)
     endif(_return_VALUE)
   
-    exec_program(${KRB5_CONFIG} ARGS --cflags gssapi RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GSSAPI_INCS)
+    execute_process(COMMAND ${KRB5_CONFIG} --cflags gssapi RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE GSSAPI_INCS)
     string(REGEX REPLACE "(\r?\n)+$" "" GSSAPI_INCS "${GSSAPI_INCS}")
     string(REGEX REPLACE " *-I" ";" GSSAPI_INCS "${GSSAPI_INCS}")
 
-    exec_program(${KRB5_CONFIG} ARGS --vendor RETURN_VALUE _return_VALUE OUTPUT_VARIABLE gssapi_flavor_tmp)
+    execute_process(COMMAND ${KRB5_CONFIG} --vendor RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE gssapi_flavor_tmp)
     set(GSSAPI_FLAVOR_MIT)
     if(gssapi_flavor_tmp MATCHES ".*Massachusetts.*")
       set(GSSAPI_FLAVOR "MIT")

cmake/modules/FindKDE4Internal.cmake
@@ -1197,7 +1197,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
    set( __KDE_HAVE_GCC_VISIBILITY ${__KDE_HAVE_GCC_VISIBILITY} CACHE BOOL "GCC support for hidden visibility")
 
    # get the gcc version
-   exec_program(${CMAKE_C_COMPILER} ARGS ${CMAKE_C_COMPILER_ARG1} --version OUTPUT_VARIABLE _gcc_version_info)
+   execute_process(COMMAND ${CMAKE_C_COMPILER} ${CMAKE_C_COMPILER_ARG1} --version OUTPUT_VARIABLE _gcc_version_info)
 
    string (REGEX MATCH "[3-9]\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
    # gcc on mac just reports: "gcc (GCC) 3.3 20030304 ..." without the patch level, handle this here:
@@ -1223,7 +1223,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
 
    set(_GCC_COMPILED_WITH_BAD_ALLOCATOR FALSE)
    if (GCC_IS_NEWER_THAN_4_1)
-      exec_program(${CMAKE_C_COMPILER} ARGS ${CMAKE_C_COMPILER_ARG1} -v OUTPUT_VARIABLE _gcc_alloc_info)
+      execute_process(COMMAND ${CMAKE_C_COMPILER} ${CMAKE_C_COMPILER_ARG1} -v OUTPUT_VARIABLE _gcc_alloc_info)
       string(REGEX MATCH "(--enable-libstdcxx-allocator=mt)" _GCC_COMPILED_WITH_BAD_ALLOCATOR "${_gcc_alloc_info}")
    endif (GCC_IS_NEWER_THAN_4_1)
 

cmake/modules/FindQt4.cmake
@@ -353,7 +353,7 @@ IF(QT_QT_LIBRARY)
 ENDIF(QT_QT_LIBRARY)
 
 function(_QT4_QUERY_QMAKE VAR RESULT)
-  exec_program(${QT_QMAKE_EXECUTABLE} ARGS "-query ${VAR}" RETURN_VALUE return_code OUTPUT_VARIABLE output )
+  execute_process(COMMAND ${QT_QMAKE_EXECUTABLE} -query ${VAR} RESULT_VARIABLE return_code OUTPUT_VARIABLE output)
   if(NOT return_code)
     file(TO_CMAKE_PATH "${output}" output)
     set(${RESULT} ${output} PARENT_SCOPE)

cmake/modules/FindSharedMimeInfo.cmake
@@ -36,7 +36,7 @@ find_program (UPDATE_MIME_DATABASE_EXECUTABLE NAMES update-mime-database)
 # Store the version number in the cache, so we don't have to search the next time again:
 if (UPDATE_MIME_DATABASE_EXECUTABLE  AND NOT  SHAREDMIMEINFO_VERSION)
 
-    exec_program (${UPDATE_MIME_DATABASE_EXECUTABLE} ARGS -v RETURN_VALUE _null OUTPUT_VARIABLE _smiVersionRaw)
+    execute_process(COMMAND ${UPDATE_MIME_DATABASE_EXECUTABLE} -v RESULT_VARIABLE _null OUTPUT_VARIABLE _smiVersionRaw)
 
     string(REGEX REPLACE "update-mime-database \\([a-zA-Z\\-]+\\) ([0-9]\\.[0-9]+).*"
            "\\1" smiVersion "${_smiVersionRaw}")

cmake/modules/FindTaglib.cmake
@@ -27,16 +27,16 @@ set(TAGLIB_CFLAGS)
 # if taglib-config has been found
 if(TAGLIBCONFIG_EXECUTABLE)
 
-  exec_program(${TAGLIBCONFIG_EXECUTABLE} ARGS --version RETURN_VALUE _return_VALUE OUTPUT_VARIABLE TAGLIB_VERSION)
+  execute_process(COMMAND ${TAGLIBCONFIG_EXECUTABLE} --version RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE TAGLIB_VERSION)
 
   if("${TAGLIB_MIN_VERSION}" VERSION_GREATER TAGLIB_VERSION)
      message(STATUS "TagLib version not found: version searched :${TAGLIB_MIN_VERSION}, found ${TAGLIB_VERSION}")
      set(TAGLIB_FOUND FALSE)
   else("${TAGLIB_MIN_VERSION}" VERSION_GREATER TAGLIB_VERSION)
 
-     exec_program(${TAGLIBCONFIG_EXECUTABLE} ARGS --libs RETURN_VALUE _return_VALUE OUTPUT_VARIABLE TAGLIB_LIBRARIES)
+     execute_process(COMMAND ${TAGLIBCONFIG_EXECUTABLE} --libs RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE TAGLIB_LIBRARIES)
 
-     exec_program(${TAGLIBCONFIG_EXECUTABLE} ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE TAGLIB_CFLAGS)
+     execute_process(COMMAND ${TAGLIBCONFIG_EXECUTABLE} --cflags RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE TAGLIB_CFLAGS)
 
      if(TAGLIB_LIBRARIES AND TAGLIB_CFLAGS)
         set(TAGLIB_FOUND TRUE)

cmake/modules/KDE4Macros.cmake
@@ -1137,7 +1137,7 @@ macro(KDE4_REMOVE_OBSOLETE_CMAKE_FILES)
    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/remove_files.cmake "#generated by cmake, don't edit\n\n")
    foreach ( _current_FILE ${ARGN})
       file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/remove_files.cmake "message(STATUS \"Removing ${module_install_dir}/${_current_FILE}\" )\n" )
-      file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/remove_files.cmake "exec_program( ${CMAKE_COMMAND} ARGS -E remove ${module_install_dir}/${_current_FILE} OUTPUT_VARIABLE _dummy)\n" )
+      file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/remove_files.cmake "execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${module_install_dir}/${_current_FILE} OUTPUT_VARIABLE _dummy)\n" )
    endforeach ( _current_FILE)
 
 endmacro(KDE4_REMOVE_OBSOLETE_CMAKE_FILES)
@@ -1189,7 +1189,7 @@ macro (KDE4_ADD_WIN32_APP_ICON appsources)
 
             # png2ico is found by the above find_program
 #            message("png2ico ${_outfilename}.ico ${ARGN}")
-            exec_program(png2ico ARGS ${_outfilename}.ico ${ARGN})
+            execute_process(COMMAND png2ico ${_outfilename}.ico ${ARGN})
 
             # now make rc file for adding it to the sources
             file(WRITE ${_outfilename}.rc "IDI_ICON1        ICON        DISCARDABLE    \"${_outfilename}.ico\"\n")

commit 00d172650a0b55915956a846146b6ece5973785f

Fix some glitches introduced with 975aebc

CreateKDELibsDependenciesFile.cmake
@@ -1,5 +1,5 @@
 # this file is included by kdelibs/CMakeLists.txt and contains the cmake code
-# which creates the KDELibsDepencies.cmake file, which is installed by kdelibs
+# which creates the KDELibsDependencies.cmake file, which is installed by kdelibs
 
 ######## write dependency file which will be installed #########
 

cmake/modules/FindGSSAPI.cmake
@@ -33,17 +33,17 @@ else(GSSAPI_LIBS AND GSSAPI_FLAVOR)
   if(KRB5_CONFIG)
   
     set(HAVE_KRB5_GSSAPI TRUE)
-    execute_process(COMMAND ${KRB5_CONFIG} --libs gssapi RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE GSSAPI_LIBS)
+    execute_process(COMMAND ${KRB5_CONFIG} --libs gssapi RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE GSSAPI_LIBS OUTPUT_STRIP_TRAILING_WHITESPACE)
     if(_return_VALUE)
       message(STATUS "GSSAPI configure check failed.")
       set(HAVE_KRB5_GSSAPI FALSE)
     endif(_return_VALUE)
   
-    execute_process(COMMAND ${KRB5_CONFIG} --cflags gssapi RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE GSSAPI_INCS)
+    execute_process(COMMAND ${KRB5_CONFIG} --cflags gssapi RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE GSSAPI_INCS OUTPUT_STRIP_TRAILING_WHITESPACE)
     string(REGEX REPLACE "(\r?\n)+$" "" GSSAPI_INCS "${GSSAPI_INCS}")
     string(REGEX REPLACE " *-I" ";" GSSAPI_INCS "${GSSAPI_INCS}")
 
-    execute_process(COMMAND ${KRB5_CONFIG} --vendor RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE gssapi_flavor_tmp)
+    execute_process(COMMAND ${KRB5_CONFIG} --vendor RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE gssapi_flavor_tmp OUTPUT_STRIP_TRAILING_WHITESPACE)
     set(GSSAPI_FLAVOR_MIT)
     if(gssapi_flavor_tmp MATCHES ".*Massachusetts.*")
       set(GSSAPI_FLAVOR "MIT")

cmake/modules/FindKDE4Internal.cmake
@@ -1197,7 +1197,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
    set( __KDE_HAVE_GCC_VISIBILITY ${__KDE_HAVE_GCC_VISIBILITY} CACHE BOOL "GCC support for hidden visibility")
 
    # get the gcc version
-   execute_process(COMMAND ${CMAKE_C_COMPILER} ${CMAKE_C_COMPILER_ARG1} --version OUTPUT_VARIABLE _gcc_version_info)
+   execute_process(COMMAND ${CMAKE_C_COMPILER} ${CMAKE_C_COMPILER_ARG1} --version OUTPUT_VARIABLE _gcc_version_info OUTPUT_STRIP_TRAILING_WHITESPACE)
 
    string (REGEX MATCH "[3-9]\\.[0-9]\\.[0-9]" _gcc_version "${_gcc_version_info}")
    # gcc on mac just reports: "gcc (GCC) 3.3 20030304 ..." without the patch level, handle this here:
@@ -1223,7 +1223,7 @@ if (CMAKE_COMPILER_IS_GNUCXX)
 
    set(_GCC_COMPILED_WITH_BAD_ALLOCATOR FALSE)
    if (GCC_IS_NEWER_THAN_4_1)
-      execute_process(COMMAND ${CMAKE_C_COMPILER} ${CMAKE_C_COMPILER_ARG1} -v OUTPUT_VARIABLE _gcc_alloc_info)
+      execute_process(COMMAND ${CMAKE_C_COMPILER} ${CMAKE_C_COMPILER_ARG1} -v OUTPUT_VARIABLE _gcc_alloc_info OUTPUT_STRIP_TRAILING_WHITESPACE)
       string(REGEX MATCH "(--enable-libstdcxx-allocator=mt)" _GCC_COMPILED_WITH_BAD_ALLOCATOR "${_gcc_alloc_info}")
    endif (GCC_IS_NEWER_THAN_4_1)
 

cmake/modules/FindQt4.cmake
@@ -353,7 +353,7 @@ IF(QT_QT_LIBRARY)
 ENDIF(QT_QT_LIBRARY)
 
 function(_QT4_QUERY_QMAKE VAR RESULT)
-  execute_process(COMMAND ${QT_QMAKE_EXECUTABLE} -query ${VAR} RESULT_VARIABLE return_code OUTPUT_VARIABLE output)
+  execute_process(COMMAND ${QT_QMAKE_EXECUTABLE} -query ${VAR} RESULT_VARIABLE return_code OUTPUT_VARIABLE output OUTPUT_STRIP_TRAILING_WHITESPACE)
   if(NOT return_code)
     file(TO_CMAKE_PATH "${output}" output)
     set(${RESULT} ${output} PARENT_SCOPE)

cmake/modules/FindSharedMimeInfo.cmake
@@ -36,7 +36,7 @@ find_program (UPDATE_MIME_DATABASE_EXECUTABLE NAMES update-mime-database)
 # Store the version number in the cache, so we don't have to search the next time again:
 if (UPDATE_MIME_DATABASE_EXECUTABLE  AND NOT  SHAREDMIMEINFO_VERSION)
 
-    execute_process(COMMAND ${UPDATE_MIME_DATABASE_EXECUTABLE} -v RESULT_VARIABLE _null OUTPUT_VARIABLE _smiVersionRaw)
+    execute_process(COMMAND ${UPDATE_MIME_DATABASE_EXECUTABLE} -v RESULT_VARIABLE _null ERROR_VARIABLE _smiVersionRaw)
 
     string(REGEX REPLACE "update-mime-database \\([a-zA-Z\\-]+\\) ([0-9]\\.[0-9]+).*"
            "\\1" smiVersion "${_smiVersionRaw}")

cmake/modules/FindTaglib.cmake
@@ -27,16 +27,16 @@ set(TAGLIB_CFLAGS)
 # if taglib-config has been found
 if(TAGLIBCONFIG_EXECUTABLE)
 
-  execute_process(COMMAND ${TAGLIBCONFIG_EXECUTABLE} --version RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE TAGLIB_VERSION)
+  execute_process(COMMAND ${TAGLIBCONFIG_EXECUTABLE} --version RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE TAGLIB_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   if("${TAGLIB_MIN_VERSION}" VERSION_GREATER TAGLIB_VERSION)
      message(STATUS "TagLib version not found: version searched :${TAGLIB_MIN_VERSION}, found ${TAGLIB_VERSION}")
      set(TAGLIB_FOUND FALSE)
   else("${TAGLIB_MIN_VERSION}" VERSION_GREATER TAGLIB_VERSION)
 
-     execute_process(COMMAND ${TAGLIBCONFIG_EXECUTABLE} --libs RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE TAGLIB_LIBRARIES)
+     execute_process(COMMAND ${TAGLIBCONFIG_EXECUTABLE} --libs RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE TAGLIB_LIBRARIES OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-     execute_process(COMMAND ${TAGLIBCONFIG_EXECUTABLE} --cflags RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE TAGLIB_CFLAGS)
+     execute_process(COMMAND ${TAGLIBCONFIG_EXECUTABLE} --cflags RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE TAGLIB_CFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
 
      if(TAGLIB_LIBRARIES AND TAGLIB_CFLAGS)
         set(TAGLIB_FOUND TRUE)